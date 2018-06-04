f46e9c78e83f1ebbf5f4389836cdf3125e26f42d
bovink <bovink@sina.com>
2018-06-04 16:52:42 +0800

    添加测试视频播放UI控件。

---
+++ Classes/ui/TestVideoPlayer.cpp
@@ -0,0 +1,13 @@
+//
+// Created by bovink on 2018/6/4.
+//
+
+#include "TestVideoPlayer.h"
+
+bool TestVideoPlayer::init() {
+    if (!Scene::init()) {
+        return false;
+    }
+
+    return true;
+}
---
+++ Classes/ui/TestVideoPlayer.h
@@ -0,0 +1,24 @@
+//
+// Created by bovink on 2018/6/4.
+//
+
+#ifndef PROJ_ANDROID_TESTVIDEOPLAYER_H
+#define PROJ_ANDROID_TESTVIDEOPLAYER_H
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class TestVideoPlayer : public Scene {
+public:
+
+    bool init() override;
+
+    CREATE_FUNC(TestVideoPlayer);
+
+
+
+};
+
+
+#endif //PROJ_ANDROID_TESTVIDEOPLAYER_H
