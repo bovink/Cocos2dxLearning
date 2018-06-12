b10bc53dfadc7efd94ee0b06f49baa7ba7de2c88
bovink <bovink@sina.com>
2018-06-12 14:38:29 +0800

    add ImageView and Text test

---
+++ Classes/ui/TestImageView.h
@@ -0,0 +1,14 @@
+//
+// Created by 吴宗刚 on 2018/6/12.
+//
+
+#ifndef PROJ_ANDROID_TESTIMAGEVIEW_H
+#define PROJ_ANDROID_TESTIMAGEVIEW_H
+
+
+class TestImageView {
+
+};
+
+
+#endif //PROJ_ANDROID_TESTIMAGEVIEW_H
