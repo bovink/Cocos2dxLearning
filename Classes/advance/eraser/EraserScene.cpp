53ca0b4767b0d4fe01b86c4a071b2c5b6201ff1c
bovink <bovink@sina.com>
2018-06-13 15:01:30 +0800

    create eraser test.

---
+++ Classes/advance/eraser/EraserScene.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 吴宗刚 on 2018/6/13.
+//
+
+#include "EraserScene.h"
+
+bool EraserScene::init() {
+    if (!Scene::init()) {
+
+        return false;
+    }
+    return true;
+}
---
+++ Classes/advance/eraser/EraserScene.h
@@ -0,0 +1,21 @@
+//
+// Created by 吴宗刚 on 2018/6/13.
+//
+
+#ifndef PROJ_ANDROID_ERASERSCENE_H
+#define PROJ_ANDROID_ERASERSCENE_H
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class EraserScene :public Scene{
+
+public:
+    CREATE_FUNC(EraserScene)
+
+    bool init() override;
+};
+
+
+#endif //PROJ_ANDROID_ERASERSCENE_H
