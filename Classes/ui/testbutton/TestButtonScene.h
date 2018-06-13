209444ffe29dad22b0b890320e2ce4cc5a3e7fde
bovink <bovink@sina.com>
2018-06-13 13:08:36 +0800

    create test button scene.

---
+++ Classes/ui/testbutton/TestButtonScene.h
@@ -0,0 +1,14 @@
+//
+// Created by 吴宗刚 on 2018/6/13.
+//
+
+#ifndef PROJ_ANDROID_TESTBUTTONSCENE_H
+#define PROJ_ANDROID_TESTBUTTONSCENE_H
+
+
+class TestButtonScene {
+
+};
+
+
+#endif //PROJ_ANDROID_TESTBUTTONSCENE_H
