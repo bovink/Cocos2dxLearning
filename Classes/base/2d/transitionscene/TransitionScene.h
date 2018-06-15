f5a1a17fff6551f3efa9ad95c384b8066b0c93ee
bovink <bovink@sina.com>
2018-06-15 15:43:46 +0800

    create transition scene test.

---
+++ Classes/base/2d/transitionscene/TransitionScene.h
@@ -0,0 +1,14 @@
+//
+// Created by 吴宗刚 on 2018/6/15.
+//
+
+#ifndef PROJ_ANDROID_TRANSITIONSCENE_H
+#define PROJ_ANDROID_TRANSITIONSCENE_H
+
+
+class TransitionScene {
+
+};
+
+
+#endif //PROJ_ANDROID_TRANSITIONSCENE_H
