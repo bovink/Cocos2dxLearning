bfa3755ff69d31d9a32d8043f3d268756081c465
bovink <bovink@sina.com>
2018-07-12 08:58:00 +0800

    修改如下： 添加用户默认数据测试。

---
+++ Classes/base/userdefault/UserDefaultScene.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 吴宗刚 on 2018/7/12.
+//
+
+#include "UserDefaultScene.h"
+
+bool UserDefaultScene::init() {
+    return BaseScene::init();
+}
---
+++ Classes/base/userdefault/UserDefaultScene.h
@@ -0,0 +1,19 @@
+//
+// Created by 吴宗刚 on 2018/7/12.
+//
+
+#ifndef PROJ_ANDROID_USERDEFAULTSCENE_H
+#define PROJ_ANDROID_USERDEFAULTSCENE_H
+
+
+#include <base/BaseScene.h>
+
+class UserDefaultScene : public BaseScene {
+public:
+    bool init() override;
+
+
+};
+
+
+#endif //PROJ_ANDROID_USERDEFAULTSCENE_H
