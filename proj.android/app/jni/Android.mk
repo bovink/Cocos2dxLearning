b325de8fc86ea4e6bb966d9073c4f10b08033450
bovink <bovink@sina.com>
2018-07-16 10:08:25 +0800

    修改如下： 修改Android.mk。

--- proj.android/app/jni/Android.mk
+++ proj.android/app/jni/Android.mk
@@ -11,23 +11,15 @@ LOCAL_MODULE := Cocos2dxLearning_shared
 
 LOCAL_MODULE_FILENAME := libCocos2dxLearning
 
-# 遍历目录及子目录的函数
-define walk
-    $(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e)))
-endef
+LOCAL_SRC_FILES := $(LOCAL_PATH)/hellocpp/main.cpp
+LOCAL_SRC_FILES += $(shell find $(LOCAL_PATH)/../../../Classes \
+                   \( -name \*.cpp -o -name \*.c \) -print)
 
-# 遍历Classes目录
-ALLFILES = $(call walk, $(LOCAL_PATH)/../../../Classes)
-
-FILE_LIST := hellocpp/main.cpp
-# 从所有文件中提取出所有.cpp文件
-FILE_LIST += $(filter %.cpp, $(ALLFILES))
-FILE_LIST += $(filter %.c, $(ALLFILES))
-
-
-LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
+LOCAL_C_INCLUDES := ${sort ${dir ${wildcard $(LOCAL_PATH)/../../../Classes/ \
+                                            $(LOCAL_PATH)/../../../Classes/*/ \
+                                            $(LOCAL_PATH)/../../../Classes/*/*/ \
+                                            $(LOCAL_PATH)/../../../Classes/*/*/*/ \
+                                            }}}
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
