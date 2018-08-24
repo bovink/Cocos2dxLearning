6d012afee75a6857f89ab3c0f5c90053baa3b5df
bovink <bovink@sina.com>
2018-08-24 09:05:39 +0800

    测试字符串居中。

--- Classes/base/ui/text/TextScene.cpp
+++ Classes/base/ui/text/TextScene.cpp
@@ -9,15 +9,28 @@ bool TextScene::init() {
         return false;
     }
 
+    auto content = Layout::create();
+    content->setBackGroundColorType(Layout::BackGroundColorType::SOLID);
+    content->setBackGroundColor(Color3B::WHITE);
+    content->setContentSize(_contentSize);
+    addChild(content);
+
     auto richText = RichText::create();
     richText->ignoreContentAdaptWithSize(false);
     richText->setContentSize(Size(_contentSize.width, 200));
 
-    auto re = RichElementText::create(1, Color3B::WHITE, 255, "看图学单词，\n点击话筒还可以跟读哦！\n单词数量：6", "fonts/fangzheng.ttf", 30);
-
+    auto re = RichElementText::create(1, Color3B::BLACK, 255, "看图学单词，\n点击话筒还可以跟读哦！\n单词数量：6",
+                                      "FZLTXIHK_1.TTF", 38);
     richText->pushBackElement(re);
     richText->setHorizontalAlignment(RichText::HorizontalAlignment::CENTER);
     richText->setPosition(_contentSize / 2);
     addChild(richText);
+
+    auto text2 = Text::create("看图学单词，\n点击话筒还可以跟读哦！\n单词数量：6",
+                              "FZLTXIHK_1.TTF", 38);
+    text2->setTextColor(Color4B::BLACK);
+    text2->setPosition(Size(_contentSize.width / 2, _contentSize.height / 2 - 100));
+    text2->setTextHorizontalAlignment(TextHAlignment::CENTER);
+    addChild(text2);
     return true;
 }
