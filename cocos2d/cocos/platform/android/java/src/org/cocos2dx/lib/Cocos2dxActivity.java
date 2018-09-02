7fc4a6695d690d750eb86773fa7a1bbc506a9592
bovink <bovink@sina.com>
2018-09-02 11:26:58 +0800

    取消虚拟导航栏的沉浸模式。

--- cocos2d/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ cocos2d/cocos/platform/android/java/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -58,10 +58,10 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
     // ===========================================================
     // Fields
     // ===========================================================
-    
+
     private Cocos2dxGLSurfaceView mGLSurfaceView = null;
     private int[] mGLContextAttrs = null;
-    private Cocos2dxHandler mHandler = null;   
+    private Cocos2dxHandler mHandler = null;
     private static Cocos2dxActivity sContext = null;
     private Cocos2dxVideoHelper mVideoHelper = null;
     private Cocos2dxWebViewHelper mWebViewHelper = null;
@@ -76,7 +76,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
     public static Context getContext() {
         return sContext;
     }
-    
+
     public void setKeepScreenOn(boolean value) {
         final boolean newValue = value;
         runOnUiThread(new Runnable() {
@@ -101,7 +101,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
             e.printStackTrace();
         }
     }
-    
+
     // ===========================================================
     // Constructors
     // ===========================================================
@@ -126,16 +126,16 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
 
         sContext = this;
         this.mHandler = new Cocos2dxHandler(this);
-        
+
         Cocos2dxHelper.init(this);
-        
+
         this.mGLContextAttrs = getGLContextAttrs();
         this.init();
 
         if (mVideoHelper == null) {
             mVideoHelper = new Cocos2dxVideoHelper(this, mFrameLayout);
         }
-        
+
         if(mWebViewHelper == null){
             mWebViewHelper = new Cocos2dxWebViewHelper(mFrameLayout);
         }
@@ -174,16 +174,16 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
 
         Cocos2dxEngineDataManager.resume();
     }
-    
+
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
     	Log.d(TAG, "onWindowFocusChanged() hasFocus=" + hasFocus);
         super.onWindowFocusChanged(hasFocus);
-        
+
         this.hasFocus = hasFocus;
         resumeIfHasFocus();
     }
-    
+
     private void resumeIfHasFocus() {
         if(hasFocus) {
             this.hideVirtualButton();
@@ -201,7 +201,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
         mGLSurfaceView.onPause();
         Cocos2dxEngineDataManager.pause();
     }
-    
+
     @Override
     protected void onDestroy() {
         Cocos2dxAudioFocusManager.unregisterAudioFocusListener(this);
@@ -217,12 +217,12 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
         msg.obj = new Cocos2dxHandler.DialogMessage(pTitle, pMessage);
         this.mHandler.sendMessage(msg);
     }
-    
+
     @Override
     public void runOnGLThread(final Runnable pRunnable) {
         this.mGLSurfaceView.queueEvent(pRunnable);
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
@@ -239,7 +239,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
     // Methods
     // ===========================================================
     public void init() {
-        
+
         // FrameLayout
         ViewGroup.LayoutParams framelayout_params =
             new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
@@ -277,7 +277,7 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
         setContentView(mFrameLayout);
     }
 
-    
+
     public Cocos2dxGLSurfaceView onCreateView() {
         Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
         //this line is need on some device if we specify an alpha bits
@@ -340,7 +340,8 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
                             | SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                             | SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                            | SYSTEM_UI_FLAG_IMMERSIVE_STICKY};
+//                            | SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    };
                     Cocos2dxReflectionHelper.<Void>invokeInstanceMethod(getWindow().getDecorView(),
                             "setSystemUiVisibility",
                             new Class[]{Integer.TYPE},
