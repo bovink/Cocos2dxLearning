c9be202f355ac2510d3e60831ac15622a0b908a4
bovink <bovink@sina.com>
2018-12-27 17:20:37 +0800

    创建下载信息类。

--- Classes/download/DownloadModule.cpp
+++ Classes/download/DownloadModule.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "DownloadModule.h"
+
+const string &DownloadInfo::getStoragePath() const {
+    return storagePath;
+}
+
+void DownloadInfo::setStoragePath(const string &storagePath) {
+    DownloadInfo::storagePath = storagePath;
+}
+
+const string &DownloadInfo::getDownloadPath() const {
+    return downloadPath;
+}
+
+void DownloadInfo::setDownloadPath(const string &downloadPath) {
+    DownloadInfo::downloadPath = downloadPath;
+}
+
+const string &DownloadInfo::getMD5() const {
+    return MD5;
+}
+
+void DownloadInfo::setMD5(const string &MD5) {
+    DownloadInfo::MD5 = MD5;
+}
+
+const string &DownloadInfo::getResourceVersion() const {
+    return resourceVersion;
+}
+
+void DownloadInfo::setResourceVersion(const string &resourceVersion) {
+    DownloadInfo::resourceVersion = resourceVersion;
+}
+
+const string &DownloadInfo::getResourceID() const {
+    return resourceID;
+}
+
+void DownloadInfo::setResourceID(const string &resourceID) {
+    DownloadInfo::resourceID = resourceID;
+}
+
+const string &DownloadInfo::getDes() const {
+    return des;
+}
+
+void DownloadInfo::setDes(const string &des) {
+    DownloadInfo::des = des;
+}
--- Classes/download/DownloadModule.h
+++ Classes/download/DownloadModule.h
@@ -5,9 +5,63 @@
 #ifndef PROJ_ANDROID_DOWNLOADMODULE_H
 #define PROJ_ANDROID_DOWNLOADMODULE_H
 
+#include <string>
 
-class DownloadModule {
+using namespace std;
 
+class DownloadHelper;
+
+class DownloadInfo;
+
+// 下载服务
+class DownlaodService {
+
+};
+
+
+// 下载帮助类
+class DownloadHelper {
+
+
+};
+
+// 下载信息
+class DownloadInfo {
+
+public:
+
+    const string &getStoragePath() const;
+
+    void setStoragePath(const string &storagePath);
+
+    const string &getDownloadPath() const;
+
+    void setDownloadPath(const string &downloadPath);
+
+    const string &getMD5() const;
+
+    void setMD5(const string &MD5);
+
+    const string &getResourceVersion() const;
+
+    void setResourceVersion(const string &resourceVersion);
+
+    const string &getResourceID() const;
+
+    void setResourceID(const string &resourceID);
+
+    const string &getDes() const;
+
+    void setDes(const string &des);
+
+private:
+
+    string storagePath;
+    string downloadPath;
+    string MD5;
+    string resourceVersion;
+    string resourceID;
+    string des;
 };
 
 
