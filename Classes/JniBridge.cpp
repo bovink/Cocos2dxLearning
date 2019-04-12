3f9c37b718f43ddc4f752fcf4cde3494f0a6eee2
bovink <bovink@sina.com>
2019-04-12 16:42:11 +0800

    创建jni交互类。

---
+++ Classes/JniBridge.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 吴宗刚 on 2019/4/12.
+//
+
+#include "JniBridge.h"
---
+++ Classes/JniBridge.h
@@ -0,0 +1,14 @@
+//
+// Created by 吴宗刚 on 2019/4/12.
+//
+
+#ifndef PROJ_ANDROID_JNIBRIDGE_H
+#define PROJ_ANDROID_JNIBRIDGE_H
+
+
+class JniBridge {
+
+};
+
+
+#endif //PROJ_ANDROID_JNIBRIDGE_H
